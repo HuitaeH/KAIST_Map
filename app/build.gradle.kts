e46f1b6d13f003ed0e568a19915a54b944b90f4e
Jaiwooyu <wodnjack@kaist.ac.kr>
2024-12-28 18:01:38 +0900

    MainActivity.kt bug fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.cardview)
     implementation(libs.veiwpager2)
+    implementation(libs.androidx.constriantlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
